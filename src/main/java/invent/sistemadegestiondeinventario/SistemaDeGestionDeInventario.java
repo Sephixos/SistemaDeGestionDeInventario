54736c299176ca56a62dd9186d0a34b3c567eb7b
ronsc22 <ronaldstivenc61@gmail.com>
2025-09-14 21:36:34 -0500

    segundo comentario

--- src/main/java/invent/sistemadegestiondeinventario/SistemaDeGestionDeInventario.java
+++ src/main/java/invent/sistemadegestiondeinventario/SistemaDeGestionDeInventario.java
@@ -12,5 +12,6 @@ public class SistemaDeGestionDeInventario {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("hello word");
     }
 }
