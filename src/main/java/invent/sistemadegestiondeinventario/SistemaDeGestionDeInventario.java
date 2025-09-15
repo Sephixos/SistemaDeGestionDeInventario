b4d06f4672414db3e8afa9b7184624e2220c8f8b
ronsc22 <ronaldstivenc61@gmail.com>
2025-09-14 21:51:00 -0500

    se crearon las clases para el patron singlenton

--- src/main/java/invent/sistemadegestiondeinventario/SistemaDeGestionDeInventario.java
+++ src/main/java/invent/sistemadegestiondeinventario/SistemaDeGestionDeInventario.java
@@ -1,15 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package invent.sistemadegestiondeinventario;
+import java.util.*;
+import modelo.Categoria;
+import modelo.Producto;
+import modelo.patrones.builder.ProductoBuilder;
+import modelo.patrones.builder.ProductoConcretoBuilder;
+import modelo.patrones.singleton.Inventario;
+import modelo.patrones.builder.Director;
 
 /**
- *
- * @author Edgar
- */
-public class SistemaDeGestionDeInventario {
+Autores: 
+Grupo 5: Edgar Bonilla, Javier Norberto Vargas Clavijo, Ronald Stiven Cardenas Aguilar
 
+Clase principal que actúa como punto de entrada de la aplicación y gestiona la
+interacción con el usuario a través de un menú de consola.
+*/
+public class SistemaDeGestionDeInventario {
+ 
+    // 1. PATRÓN SINGLETON
+    // Se obtiene la instancia Única del inventario utilizando el patrón Singleton.
+    // Todas las operaciones de la aplicación se realizarán sobre esta Única instancia.
+    private static final Inventario inventario = Inventario.getInstance();
+    
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
         System.out.println("hello word");
--- src/main/java/modelo/Categoria.java
+++ src/main/java/modelo/Categoria.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package modelo;
 
+package modelo;
+import java.util.*;
 /**
- *
- * @author Edgar
- */
+Autores: 
+Grupo 5: Edgar Bonilla, Javier Norberto Vargas Clavijo, Ronald Stiven Cardenas Aguilar
+ 
+Clase que representa una categor�a de productos.
+Contiene informaci�n general y los atributos espec�ficos que los productos
+de esta categor�a deben tener.
+*/
+
 public class Categoria {
-    
+     private String nombre;
+    private String descripcion;
+    // Lista de nombres de atributos, ej: "Talla", "Color", "Voltaje".
+    private List<String> atributosEspecificos;
+
+    public Categoria(String nombre, String descripcion, List<String> atributosEspecificos) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.atributosEspecificos = atributosEspecificos;
+    }
+
+    // Getters para acceder a las propiedades de la categor�a.
+    public String getNombre() {
+        return nombre;
+    }
+    // Obtiene la descripci�n de la categor�a
+    public String getDescripcion() {
+        return descripcion;
+    }
+    // Obtiene los atributos
+    public List<String> getAtributosEspecificos() {
+        return atributosEspecificos;
+    }
+    // Sobreescribe el m�todo toString()
+    @Override
+    public String toString() {
+        return "Categoria [nombre=" + nombre + ", descripcion=" + descripcion + "]";
+    }
 }
